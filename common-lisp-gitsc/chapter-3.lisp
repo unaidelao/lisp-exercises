7dd88c4c502a68e36ee9735ecda339e8139d9d60
unaidelao <unai.delao@gmail.com>
2019-04-18 09:03:15 +0200

    Exercises from 3.10 to 3.21

--- common-lisp-gitsc/chapter-3.lisp
+++ common-lisp-gitsc/chapter-3.lisp
@@ -52,3 +52,117 @@
    (third '(1 foo 2 moo))) ; 6
 
 (rest '(cons is short for construct)) ; (IS SHORT FOR CONSTRUCT)
+
+
+;;; 3.10. The following expressions all result in errors. Write down the type of error that occurs,
+;;; explain how the error arose (for example, missing quote, quote in wrong place), and correct the
+;;; expression by changing only the quotes.
+(third (the quick brown fox)) ; correct: (third '(the quick brown fox))
+
+(list 2 and 2 is 4) ; correct: (list 2 'and 2 'is 4)
+
+(+ 1 '(length (list t t t t))) ; correct: (+ 1 (length (list t t t t)))
+
+(cons 'patrick (seymour marvin)) ; correct: (cons 'patrick '(seymour marvin))
+
+
+;;; 3.11. Define a predicate called LONGER-THAN that takes two lists as input and returns T if the
+;;; first list is longer than the second.
+(defun longer-than (list1 list2)
+  (> (length list1) (length list2)))
+
+
+;;; 3.12. Write a function ADDLENGTH that takes a list as input and returns a new list with the
+;;; length of the input added onto the front of it. If the input is (MOO GOO GAI PAN), the output
+;;; should be (4 MOO GOO GAI PAN).
+;;;
+;;; What is the result of (ADDLENGTH (ADDLENGTH '(A B C)))?
+(defun addlength (x)
+  (cons (length x) x))
+
+;; Result of (ADDLENGTH (ADDLENGTH '(A B C))) -> (4 3 A B C)
+
+
+;;; 3.13. Study this function definition:
+(defun call-up (caller callee)
+  (list 'hello callee 'this 'is caller 'calling))
+
+;; How many arguments does this function require?
+; Answer: This function requires 2 arguments.
+
+;; What are the names of the arguments?
+; Answer: CALLER and CALLEE.
+
+;; What is the result of (CALL-UP 'FRED 'WANDA)?
+; Answer: (HELLO WANDA THIS IS FRED CALLING)
+
+
+;;; 3.14. Here is a variation on the CALL-UP function from the previous problem.
+(defun crank-call (caller callee)
+  '(hello callee this is caller calling))
+
+;; What is the result of (CRANK-CALL 'WANDA 'FRED)?
+; Answer: (HELLO CALLEE THIS IS CALLER CALLING)
+
+
+;;; 3.15. Consider the following function, paying close attention to the quotes:
+(defun scrabble (word)
+  (list word 'is 'a 'word))
+
+;; The symbol WORD is used two different ways in this function. What are they?
+; Answer: Used without quote, the symbol is used as a variable. With quote, is used as data.
+
+;; What is the result of (SCRABBLE 'AARDVARK)?
+; Answer: (AARDVARK IS A WORD)
+
+;; What is the result of (SCRABBLE 'WORD)?
+; Answer: (WORD IS A WORD)
+
+
+;;; 3.16. Here's a real confuser:
+(defun stooge (larry moe curly)
+  (list larry (list 'moe curly) curly 'larry))
+
+;; What does the following evaluate to?: (stooge 'moe 'curly 'larry)
+; Answer: (MOE (MOE LARRY) LARRY LARRY)
+
+
+;;; 3.17. Why can’t the special symbols T or NIL be used as variables in a function definition?
+; Answer: Because T and NIL evaluates themselves, and because they are reserved words in LISP.
+
+
+;;; 3.19. Evaluate each of the following lists. If the list causes an error, tell what
+;;; the error is. Otherwise, write the result of the evaluation.
+(cons 'grapes '(of wrath)) ; (GRAPES OF WRATH)
+
+(list t 'is 'not nil) ; (T IS NOT NIL)
+
+(first '(list moose goose)) ; LIST
+
+(first (list 'moose 'goose)) ; MOOSE
+
+(cons 'home ('sweet 'home)) ; ERROR: ILLEGAL FUNCTION CALL 'SWEET
+
+
+;;; 3.20. Here is a mystery function:
+(defun mystery (x)
+  (list (second x) (first x)))
+
+;; What result or error is produced by evaluating each of the following?
+(mystery '(dancing bear)) ; (BEAR DANCING)
+
+(mystery 'dancing 'bear) ; INVALID NUMBER OF ARGUMENTS: 2
+
+(mystery '(zowie)) ; (NIL ZOWIE)
+
+(mystery (list 'first 'second)) ; (SECOND FIRST)
+
+
+;;; 3.21. What is wrong with each of the following function definitions?
+(defun speak (x y) (list 'all 'x 'is 'y)) ; Answer: Variables X and Y not used. (Don't quote variables!).
+
+(defun speak (x) (y) (list 'all x 'is y)) ; Answer: Variable X not used and Undefined variable Y. (Function must have only one argument list!).
+
+(defun speak ((x) (y)) (list all 'x is 'y)) ; Answer: Required argument is not a symbol (Remove variables parenthesis!). ALL and IS must be quoted, not the variables.
+
+

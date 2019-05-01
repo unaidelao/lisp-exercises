4a93af4d32b9187c5a675e11a7964bea37447082
unaidelao <unai.delao@gmail.com>
2019-05-01 23:50:56 +0200

    Exercise 5.6.a and 5.6.b

--- common-lisp-gitsc/chapter-5.lisp
+++ common-lisp-gitsc/chapter-5.lisp
@@ -29,3 +29,23 @@
 ;;; 5.5. Are LET and LET* equivalent when you are only creating one local variable?
 ; Answer: Yes, indeed. They are not equivalent when creating several local variables: LET
 ; performs the bindings in parallel, LET* does them sequentially.
+
+
+;;; 5.6. This keyboard exercise is about dice. We will start with a function to throw one dice and
+;;; end up with a program to play craps. Be sure to include a documentation string for each
+;;; function you write.
+
+;; a) Write a function THROW-DIcE that returns a random number from 1 to 6, inclusive.
+;; Remember that (RANDOM 6) will pick numbers from 0 to 5. THROW-DIE doesn’t need any inputs,
+;; so its argument list should be NIL.
+(defun throw-dice ()
+"Returns a random number from 1 to 6."
+  (+ 1 (RANDOM 6)))
+
+;; b) Write a function THROW-DICES that throws two dices and returns a list of two numbers:
+;; the value of the first die and the value of the second. We’ll call this list a "throw".
+;; For example, (THROW-DICES) might return the throw (3 5), indicating that the first dice
+;; was a 3 and the second a 5.
+(defun throw-dices ()
+"Returns a list with two THROW-DICE results."
+  (list (throw-dice) (throw-dice)))
